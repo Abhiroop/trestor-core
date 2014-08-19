e1f2796a4e15a32e35dd84900db8cf7c1bcef092
aritradhar <aritra.dhar7@gmail.com>
2014-08-20 00:59:10 +0530

    UI

---
+++ TNetWallet/HomeScreen.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TNetWallet
+{
+    /// <summary>
+    /// Interaction logic for HomeScreen.xaml
+    /// </summary>
+    public partial class HomeScreen : Page
+    {
+        public HomeScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void button_CreateAccount_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new RegisterPage());
+        }
+
+        private void button_Login_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
