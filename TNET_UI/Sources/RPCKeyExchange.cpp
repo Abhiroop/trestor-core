615f2b3b944b846880b17d9c0488a8e12214fed2
aritradhar <aritra.dhar7@gmail.com>
2014-11-28 02:51:20 +0530

    key exchange 4

--- TNET_UI/Sources/RPCKeyExchange.cpp
+++ TNET_UI/Sources/RPCKeyExchange.cpp
@@ -21,11 +21,11 @@ web::json::value RPCKeyExchange::initExchange()
 	ed25519_create_seed(seed);
 	ed25519_create_seed(Token);
 
-	Hash token(Token, Token + 32);
+	Hash token(Token, Token + 8);
 
 	ed25519_create_keypair(privateKey, publicKey, seed);
 
-	string encoeToken = base64_encode_2((const char*)Token, 32);
+	string encoeToken = base64_encode_2((const char*)Token, 8);
 	std::wstring wt;
 	wt.assign(encoeToken.begin(), encoeToken.end());
 	jsonToSend[L"token"] = json::value::string(wt);
@@ -85,8 +85,9 @@ web::json::value RPCKeyExchange::handleKetExchange(http_request request)
 						  unsigned char seed[32];
 						  unsigned char ownSessionPk[32];
 						  unsigned char ownSessionSk[32];
-						  unsigned char sessionToken[32];
-						  
+						  unsigned char sessionToken[8];
+						  string randomToken;
+
 						  unsigned char sharedSessionKey[32];
 						  
 						  if (jvalue.has_field(L"token"))
@@ -94,7 +95,6 @@ web::json::value RPCKeyExchange::handleKetExchange(http_request request)
 							  value token = jvalue[L"token"];
 							  if (token.is_string())
 							  {
-								  string randomToken;
 								  randomToken.assign(token.as_string().begin(), token.as_string().end());
 								  string decodedRandomToken = base64_decode_2(randomToken);
 								  
@@ -127,10 +127,10 @@ web::json::value RPCKeyExchange::handleKetExchange(http_request request)
 									  break;
 
 								  //shared secret
-								  byte b[32];
+								  byte otherPK[32];
 								  for (int i = 0; i < (int)decodedPK.size(); i++)
 								  {
-									  b[i] = (byte)decodedPK[i];
+									  otherPK[i] = (byte)decodedPK[i];
 								  }
 
 								  //generate shared secret here
@@ -138,7 +138,7 @@ web::json::value RPCKeyExchange::handleKetExchange(http_request request)
 								  unsigned char shared_secret[32];
 								  unsigned char temp_hash[64];
 
-								  ed25519_key_exchange(shared_secret, b, ownSessionSk);
+								  ed25519_key_exchange(shared_secret, otherPK, ownSessionSk);
 								  sha512(shared_secret, 32, temp_hash);
 
 								  memcpy(sharedSessionKey, temp_hash, 32);
@@ -155,16 +155,20 @@ web::json::value RPCKeyExchange::handleKetExchange(http_request request)
 
 								  unsigned char signedSessionPk[64];
 								  ed25519_sign(signedSessionPk, ownSessionPk, 32, state.PublicKey.data(), state.PrivateKey.data());
+
 								  string encodedSignature = base64_encode_2((const char*)signedSessionPk, 64);
 								  std::wstring wsg;
 								  wsg.assign(encodedSignature.begin(), encodedSignature.end());
+								  jsonToSend[L"signature"] = json::value::string(wsg);
 
 								  string encodedOwnIdentityPK = base64_encode_2((const char*)state.PublicKey.data(), 32);
 								  std::wstring woipk;
 								  woipk.assign(encodedOwnIdentityPK.begin(), encodedOwnIdentityPK.end());
 								  jsonToSend[L"publicKey"] = json::value::string(woipk);
 
-								  jsonToSend[L"signature"] = json::value::string(wsg);
+								  std::wstring wrt;
+								  wrt.assign(randomToken.begin(), randomToken.end());
+								  jsonToSend[L"token"] = json::value::string(wrt);
 
 								  //register all data in the hashmap
 								  concurrent_hash_map<Hash, KeyExchangeState>::accessor acc;
@@ -176,6 +180,8 @@ web::json::value RPCKeyExchange::handleKetExchange(http_request request)
 									  Hash sk(ownSessionSk, ownSessionSk + 32);
 									   
 									  KeyExchangeState KES(2, sk, pk);
+									  Hash sharedSecretHash(shared_secret, shared_secret + 32);
+									  KES.sharedSecret = sharedSecretHash;
 
 									  keyExchangeStateMap.insert(make_pair(token, KES));
 								  }
@@ -193,7 +199,31 @@ web::json::value RPCKeyExchange::handleKetExchange(http_request request)
 						  unsigned char otherPublicKey[32];
 						  unsigned char signature[64];
 						  unsigned char otherSessionPublicKey[32];
+						  unsigned char token[8];
+						  unsigned char sharedSessionKey[32];
+						  string randomToken;
 
+						  if (jvalue.has_field(L"token"))
+						  {
+							  value tk = jvalue[L"token"];
+
+							  if (tk.is_string())
+							  {
+								  
+								  randomToken.assign(tk.as_string().begin(), tk.as_string().end());
+								  string decodedTK = base64_decode_2(randomToken);
+
+								  if (decodedTK.size() != 8)
+									  break;
+
+								  //other's public key
+								  for (int i = 0; i < (int)decodedTK.size(); i++)
+								  {
+									  token[i] = (byte)decodedTK[i];
+								  }
+							  }
+						  }
+						  
 						  if (jvalue.has_field(L"publicKey"))
 						  {
 							  value pk = jvalue[L"publicKey"];
@@ -207,17 +237,143 @@ web::json::value RPCKeyExchange::handleKetExchange(http_request request)
 								  if (decodedPK.size() != 32)
 									  break;
 
-								  //shared secret
+								  //other's public key
 								  for (int i = 0; i < (int)decodedPK.size(); i++)
 								  {
 									  otherPublicKey[i] = (byte)decodedPK[i];
 								  }
-
-
 							  }
 						  }
+
+						  if (jvalue.has_field(L"sessionPublicKey"))
+						  {
+							  value spk = jvalue[L"sessionPublicKey"];
+
+							  if (spk.is_string())
+							  {
+								  string sessionPublicKey;
+								  sessionPublicKey.assign(spk.as_string().begin(), spk.as_string().end());
+								  string decodedSPK = base64_decode_2(sessionPublicKey);
+
+								  if (decodedSPK.size() != 32)
+									  break;
+
+								  //other's session public key
+								  for (int i = 0; i < (int)decodedSPK.size(); i++)
+								  {
+									  otherSessionPublicKey[i] = (byte)decodedSPK[i];
+								  }
+							  }
+						  }
+
+						  if (jvalue.has_field(L"signature"))
+						  {
+							  value sig = jvalue[L"signature"];
+
+							  if (sig.is_string())
+							  {
+								  string signature;
+								  signature.assign(sig.as_string().begin(), sig.as_string().end());
+								  string decodedSIG = base64_decode_2(signature);
+
+								  if (decodedSIG.size() != 64)
+									  break;
+
+								  //other's session public key
+								  for (int i = 0; i < (int)decodedSIG.size(); i++)
+								  {
+									  signature[i] = (byte)decodedSIG[i];
+								  }
+							  }
+							  //verify the signature
+
+							  int ver = ed25519_verify(signature, otherSessionPublicKey, 32, otherPublicKey);
+							  
+							  if (ver == 0)
+								  break;
+						  }
+
+						  //get own key from the token
+						  concurrent_hash_map<Hash, KeyExchangeState>::accessor acc;
+						  KeyExchangeState KES;
+
+						  Hash tokenHash(token, token + 8);
+						  if (keyExchangeStateMap.find(acc, tokenHash))
+						  {
+							  KES = acc->second;
+						  }
+
+						  else
+						  {
+							  break;
+						  }
+						  
+
+						  //generate shared secret here
+
+						  unsigned char shared_secret[32];
+						  unsigned char temp_hash[64];
+
+						  ed25519_key_exchange(shared_secret, otherPublicKey, KES.privateKey.data());
+						  sha512(shared_secret, 32, temp_hash);
+
+						  memcpy(sharedSessionKey, temp_hash, 32);
+
+						  
+						  //make the json object and send own identity public key along withh a signature
+						  //also do a wrap up
+						  Hash sharedSessionKeyHash(sharedSessionKey, sharedSessionKey + 32);
+						  Hash otherPublicKeyHash(otherPublicKey, otherPublicKey + 32);
+						  //delete
+						  keyExchangeStateMap.erase(tokenHash);
+
+						  concurrent_hash_map<Hash, Hash>::accessor acc1;
+						  //entry
+						  if (!exchangedKey.find(acc1, tokenHash))
+						  {
+							  exchangedKey.insert(make_pair(otherPublicKeyHash, sharedSessionKeyHash));
+						  }
+						  //else ignore
+
+						  //json object
+						  value jsonToSend;
+
+						  jsonToSend[L"state"] = json::value::number(3);
+
+						  std::wstring wrt;
+						  wrt.assign(randomToken.begin(), randomToken.end());
+						  jsonToSend[L"token"] = json::value::string(wrt);
+
+						  std::wstring woipk;
+						  string encodedOwnIdentityPublicKey = base64_encode_2((const char*) state.PublicKey.data(), 32);
+						  woipk.assign(encodedOwnIdentityPublicKey.begin(), encodedOwnIdentityPublicKey.end());
+						  jsonToSend[L"publicKey"] = json::value::string(woipk);
+
+
+						  unsigned char signedSessionPk[64];
+						  ed25519_sign(signedSessionPk, KES.publicKey.data(), 32, state.PublicKey.data(), state.PrivateKey.data());
+
+						  string encodedSignature = base64_encode_2((const char*)signedSessionPk, 64);
+						  std::wstring wsg;
+						  wsg.assign(encodedSignature.begin(), encodedSignature.end());
+						  jsonToSend[L"signature"] = json::value::string(wsg);
+
+						  return jsonToSend;
+
 				}
 
+					//final state of the key exchange
+					//wrap up all the data structures
+				case 3:
+				{
+
+				}
+
+					
+				default:
+				{
+						   throw exception("Die bitch die!!!");
+				}
 
 				}
 
