e732148b8b4bcdd4ef6e81f28b4b6465f6b07a2b
Stephan Verbuecheln <stephan@trestor.org>
2015-06-12 00:29:55 +0530

    gossip msg handler

--- tnetd/Network/PeerDiscovery/PeerDiscovery.cs
+++ tnetd/Network/PeerDiscovery/PeerDiscovery.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TNetD.Network;
+using TNetD.Network.Networking;
 
 namespace TNetD.Network.PeerDiscovery
 {
     class PeerDiscovery
     {
+
+        public PeerDiscovery()
+        {
+
+        }
+
+        public void InitiateGossip(Hash node)
+        {
+
+        }
+
+        public void GossipMsgHandler(NetworkPacket packet)
+        {
+            switch (packet.Type)
+            {
+                case PacketType.TPT_GOSSIP_INIT:
+                    respondToGossip(packet);
+                    return;
+                case PacketType.TPT_GOSSIP_RESPONSE:
+                    processGossipResponse(packet);
+                    return;
+            }
+        }
+
+        private void respondToGossip(NetworkPacket packet)
+        {
+
+        }
+
+        private void processGossipResponse(NetworkPacket packet)
+        {
+
+        }
     }
 }
