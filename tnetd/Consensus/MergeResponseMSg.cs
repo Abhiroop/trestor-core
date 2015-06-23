c98304591a1f202c56b5fc63625db4718ad89f37
Stephan Verbuecheln <stephan@trestor.org>
2015-06-24 01:57:39 +0530

    mergemsg

--- tnetd/Consensus/MergeResponseMSg.cs
+++ tnetd/Consensus/MergeResponseMSg.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TNetD.Protocol;
 
 namespace TNetD.Consensus
 {
-    class MergeResponseMsg
+    class MergeResponseMsg : ISerializableBase
     {
+        public SortedSet<Hash> transactions;
+
+        public MergeResponseMsg()
+        {
+            transactions = new SortedSet<Hash>();
+        }
+
+        public byte[] Serialize()
+        {
+            ProtocolDataType[] PDTs = new ProtocolDataType[transactions.Count];
+            int i = 0;
+            foreach (Hash transaction in transactions)
+            {
+                PDTs[i] = ProtocolPackager.Pack(transaction, 0);
+                i++;
+            }
+            return ProtocolPackager.PackRaw(PDTs);
+        }
+
+        public void Deserialize(byte[] data)
+        {
+            List<ProtocolDataType> PDTs = ProtocolPackager.UnPackRaw(data);
+            foreach (ProtocolDataType p in PDTs)
+            {
+                Hash t = new Hash();
+                ProtocolPackager.UnpackHash(p, 0, out t);
+                transactions.Add(t);
+            }
+        }
     }
 }
