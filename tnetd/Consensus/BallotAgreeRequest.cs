c58aa952e6a0d8f8c9e5d04c8856e292ea8d16ba
Arpan Jati <arpanj@iiitd.ac.in>
2015-06-24 21:03:39 +0530

    Ballot Agree Request Messages

--- tnetd/Consensus/BallotAgreeRequest.cs
+++ tnetd/Consensus/BallotAgreeRequest.cs
@@ -1,12 +1,68 @@
-﻿using System;
+﻿
+//  @Author: Arpan Jati
+//  @Date: 24 June 2015 
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TNetD.Protocol;
 
 namespace TNetD.Consensus
 {
-    class BallotAgreeRequest
+    class BallotAgreeRequest : ISerializableBase
     {
+        public Hash PublicKey;
+        public Hash BallotHash;
+        public long LedgerCloseSequence;
+        
+        public BallotAgreeRequest()
+        {
+            Init();
+        }
+
+        public void Init()
+        {
+            PublicKey = new Hash();
+            PublicKey = new Hash();
+            LedgerCloseSequence = 0;
+        }
+
+        public byte[] Serialize()
+        {
+            List<ProtocolDataType> PDTs = new List<ProtocolDataType>();
+            PDTs.Add(ProtocolPackager.Pack(PublicKey, 0));
+            PDTs.Add(ProtocolPackager.Pack(BallotHash, 1));
+            PDTs.Add(ProtocolPackager.Pack(LedgerCloseSequence, 2));
+            return ProtocolPackager.PackRaw(PDTs);
+        }
+
+        public void Deserialize(byte[] Data)
+        {
+            Init();
+
+            List<ProtocolDataType> PDTs = ProtocolPackager.UnPackRaw(Data);            
+
+            foreach(ProtocolDataType PDT in PDTs)
+            {
+                switch (PDT.NameType)
+                {
+                    case 0:
+                        ProtocolPackager.UnpackHash(PDT, 0, out PublicKey);                        
+                        break;
+
+                    case 1:
+                        ProtocolPackager.UnpackHash(PDT, 1, out BallotHash);
+                        break;
+
+                    case 2:
+                        ProtocolPackager.UnpackInt64(PDT, 2, ref LedgerCloseSequence);
+                        break;
+                }
+            }
+        }
+        
+
     }
 }
--- tnetd/Consensus/BallotAgreeResponse.cs
+++ tnetd/Consensus/BallotAgreeResponse.cs
@@ -1,12 +1,77 @@
-﻿using System;
+﻿
+//  @Author: Arpan Jati
+//  @Date: 24 June 2015 
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TNetD.Protocol;
 
 namespace TNetD.Consensus
 {
-    class BallotAgreeResponse
+    class BallotAgreeResponse : ISerializableBase
     {
+        
+        public Hash SignerPublicKey;
+        public Hash BallotHash;
+        public long LedgerCloseSequence;
+        public Hash Signature;
+
+        public BallotAgreeResponse()
+        {
+            Init();
+        }
+
+        public void Init()
+        {
+            SignerPublicKey = new Hash();
+            BallotHash = new Hash();
+            Signature = new Hash();
+            LedgerCloseSequence = 0;
+        }
+
+        public byte[] Serialize()
+        {
+            List<ProtocolDataType> PDTs = new List<ProtocolDataType>();
+            PDTs.Add(ProtocolPackager.Pack(SignerPublicKey, 0));
+            PDTs.Add(ProtocolPackager.Pack(BallotHash, 1));
+            PDTs.Add(ProtocolPackager.Pack(Signature, 2));
+            PDTs.Add(ProtocolPackager.Pack(LedgerCloseSequence, 3));
+            return ProtocolPackager.PackRaw(PDTs);
+        }
+
+        public void Deserialize(byte[] Data)
+        {
+            Init();
+
+            List<ProtocolDataType> PDTs = ProtocolPackager.UnPackRaw(Data);            
+
+            foreach(ProtocolDataType PDT in PDTs)
+            {
+                switch (PDT.NameType)
+                {
+                    case 0:
+                        ProtocolPackager.UnpackHash(PDT, 0, out SignerPublicKey);                        
+                        break;
+
+                    case 1:
+                        ProtocolPackager.UnpackHash(PDT, 1, out BallotHash);
+                        break;
+
+                    case 2:
+                        ProtocolPackager.UnpackHash(PDT, 2, out Signature);
+                        break;
+
+                    case 3:
+                        ProtocolPackager.UnpackInt64(PDT, 3, ref LedgerCloseSequence);
+                        break;
+                }
+            }
+        }
+
+       // public 
+
     }
 }
