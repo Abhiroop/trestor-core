f6b54abf1b52462b2bddb32a9ef99b9971c03009
Arpan Jati <arpanj@iiitd.ac.in>
2015-06-24 16:21:40 +0530

    BallotReqMsg

--- tnetd/Consensus/BallotRequestMessage.cs
+++ tnetd/Consensus/BallotRequestMessage.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TNetD.Protocol;
 
 namespace TNetD.Consensus
 {
-    class BallotRequestMessage
+    class BallotRequestMessage : ISerializableBase
     {
+        public long LedgerCloseSequence;
+
+        public BallotRequestMessage()
+        {
+            LedgerCloseSequence = 0;
+        }
+
+        public byte[] Serialize()
+        {
+            ProtocolDataType[] PDTs = new ProtocolDataType[1];
+            PDTs[0] = ProtocolPackager.PackVarint(LedgerCloseSequence, 0);
+            return ProtocolPackager.PackRaw(PDTs);
+        }
+
+        public void Deserialize(byte[] data)
+        {
+            List<ProtocolDataType> PDTs = ProtocolPackager.UnPackRaw(data);
+
+            if (PDTs.Count == 1)
+            {
+                long lcs = 0;
+                if (ProtocolPackager.UnpackVarint(PDTs[0], 0, ref lcs))
+                {
+                    LedgerCloseSequence = lcs;
+                }
+            }
+
+        }
     }
 }
