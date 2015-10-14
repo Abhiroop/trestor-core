ae833c72466cd4a3fbaccbd32f28d8da3691cf62
Stephan Verbuecheln <erlenmayr@gmail.com>
2015-10-14 08:32:30 +0200

    logging

--- tnetd/Nodes/Logging.cs
+++ tnetd/Nodes/Logging.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TNetD.Nodes
 {
     class Logging
     {
+
+        private FileStream file;
+
+        /// <summary>
+        /// creates a logging class for logging server activity
+        /// 
+        /// will log to filename logs/name.log for a given name
+        /// </summary>
+        /// <param name="name">name for the log file</param>
+        public Logging(string name)
+        {
+            file = new FileStream("logs/" + name + ".log", FileMode.Create);
+        }
+
+        public void Log(string method, string message)
+        {
+            StreamWriter w = new StreamWriter(file, Encoding.UTF8);
+            w.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + " [" + method + "] " + message);
+        }
     }
 }
