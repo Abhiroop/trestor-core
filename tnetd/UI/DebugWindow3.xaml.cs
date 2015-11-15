a7ac46b3ef1318d878e63b34d67e2bd69226ef86
Stephan Verbuecheln <stephan@trestor.org>
2015-11-15 14:27:09 +0100

    ledgersync debug

--- tnetd/UI/DebugWindow3.xaml.cs
+++ tnetd/UI/DebugWindow3.xaml.cs
@@ -87,6 +87,7 @@ namespace TNetD
             nd.BeginBackgroundLoad();
 
             nodes.Add(nd);
+            nd.LedgerSyncEnabled = true;
         }
 
         void nd_NodeStatusEvent(string Status, int NodeID)
@@ -176,7 +177,7 @@ namespace TNetD
 
             long received_takas = 0;
 
-            for (int i = 0; i < 5000; i++)
+            for (int i = 0; i < count; i++)
             {
                 byte[] N_H = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 };
                 Common.SECURE_RNG.GetBytes(N_H);
