42c1ddcd7d0e1f2a511da7ca4e71412a770ec365
Abhiroop <asiamgenius@gmail.com>
2016-02-03 13:38:25 +0530

    An extremely high resolution timer using kernel call

---
+++ tnetd/Utils/Helpers/HighResolutionDateTime.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace TNetD.Helpers
+{
+    public static class HighResolutionDateTime
+    {
+        public static bool IsAvailable { get; private set; }
+
+        [DllImport("Kernel32.dll", CallingConvention = CallingConvention.Winapi)]
+        private static extern void GetSystemTimePreciseAsFileTime(out long filetime);
+
+        public static long UtcNow
+        {
+            get
+            {
+                if(!IsAvailable)
+                {
+                    throw new InvalidOperationException("High Resolution Clock isn't available");
+                }
+
+                long filetime;
+                GetSystemTimePreciseAsFileTime(out filetime);
+
+                return filetime;
+            }
+        }
+
+        static HighResolutionDateTime()
+        {
+            try
+            {
+                long filetime;
+                GetSystemTimePreciseAsFileTime(out filetime);
+                IsAvailable = true;
+            }
+            catch (EntryPointNotFoundException)
+            {             
+                // Not running Windows 8 or higher.             
+                IsAvailable = false;
+            }
+        }
+    }
+}
